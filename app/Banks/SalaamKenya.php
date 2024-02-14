1b23e4fbb3362e29161389f6a1b524e387fd6742
noorfarooqy <noor@drongo.vip>
2024-02-14 23:27:22 +0300

    update the opoening balance

--- app/Banks/SalaamKenya.php
+++ app/Banks/SalaamKenya.php
@@ -2,6 +2,7 @@
 
 namespace Noorfarooqy\BankGateway\Banks;
 
+use Noorfarooqy\Flexcube\Services\FlexcubeServices;
 use Noorfarooqy\NoorAuth\Traits\ResponseHandler;
 
 class SalaamKenya extends Bank
@@ -9,8 +10,20 @@ class SalaamKenya extends Bank
     use ResponseHandler;
     public function getBalance($account, $branch = null): object
     {
-        // TODO: Implement getBalance() method.
-        return $this->getResponse();
+        $flexcubeServices = new FlexcubeServices();
+        $balance = $flexcubeServices->AccountBalance($account, $branch);
+        if (!$balance) {
+            $this->setError($flexcubeServices->getMessage());
+            return $this->getResponse();
+        }
+        return $this->getResponse([
+            'account' => $account,
+            'branch' => $balance->{'BRANCH_CODE'},
+            'opening_valance' => $balance->{'OPNBAL'},
+            'current_balance' => $balance->{'CURBAL'},
+            'available_balance' => $balance->{'AVLBAL'},
+            'block_amount' => $balance->{'ACY_BKD_AMT'},
+        ]);
     }
 
     public function getAccountDetails(): object
