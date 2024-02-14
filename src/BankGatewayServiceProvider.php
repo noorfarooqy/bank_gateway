cb4400dbd5971b82005fee59bce7622a91af8eb7
noorfarooqy <noor@drongo.vip>
2024-02-14 22:40:06 +0300

    enable route api

--- src/BankGatewayServiceProvider.php
+++ src/BankGatewayServiceProvider.php
@@ -9,6 +9,11 @@ class BankGatewayServiceProvider extends ServiceProvider
 
     public function boot()
     {
+
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->publishes([
+            __DIR__ . '/../config/bankgateway.php' => config_path('bankgateway.php'),
+        ], 'bankgateway-config');
     }
     public function register()
     {
